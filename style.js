7ff96c3229adc3fb9d8fb4c2ce9709ced6bbe48d
0piiu <111980835+0piiu@users.noreply.github.com>
2022-10-19 07:42:44 -0300

    Add files via upload

--- style.js
+++ style.js
@@ -1,1 +1,8 @@
-alert(if (true) {})
+var resposta = confirm('deseja continuar na pagína?');
+        console.log(resposta); 
+
+document.querySelector("#matricula").addEventListener("click",function () {
+        alert('No momento estamos enfrentado alguns problemas, por isso não será possível realizar a matricula.')
+});
+//var resposta = confirm('como vc esta?')
+        console.log(resposta)
